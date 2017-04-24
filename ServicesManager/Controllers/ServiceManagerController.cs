6aac7179ceb692128379a009c8e024b9161bf0e7
Osvaldo Daibert <osvaldo.daibert@microsoft.com>
2017-04-24 10:41:49 -0700

    Adding new files

---
+++ ServicesManager/Controllers/ServiceManagerController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric.Health;
+using System.Fabric.Query;
+using System.IO;
+using System.Net;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+using System.Fabric;
+using System.Threading.Tasks;
+using System.Fabric.Description;
+using System.Text;
+using Microsoft.ServiceFabric;
+using ServiceFabricServiceModel;
+
+namespace ServicesManager.Controllers
+{
+    [Route("api/ServiceManager")]
+    public class ServiceManagerController : ApiController
+    {
+        private readonly StatelessServiceContext context;
+
+        public ServiceManagerController()
+        {
+          
+        }
+
+        public ServiceManagerController(FabricClient fabricClient, StatelessServiceContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<string> Get()
+        {
+            CreateService("fabric:/MailboxService"); //this.context.CodePackageActivationContext.ApplicationName;
+
+            yield return "200";
+        }
+        private static FabricClient _fabricClient =new FabricClient();
+
+        private Task CreateService(string appName)
+        {
+            StatelessServiceDescription serviceDescription = new StatelessServiceDescription()
+            {
+                ApplicationName = new Uri(appName),
+                PartitionSchemeDescription = new SingletonPartitionSchemeDescription(),
+                InstanceCount = 1,
+                ServiceTypeName = "MailboxType",
+                ServiceName = new Uri($"{appName}/Mailbox" + new Random().Next(100).ToString() )
+            };
+            return _fabricClient.ServiceManager.CreateServiceAsync(serviceDescription);
+        }
+
+        public string Get(int instances)
+        {
+            var fc = new FabricClient();
+            string appName = this.context.CodePackageActivationContext.ApplicationName;
+
+            StatelessServiceDescription serviceDescription = new StatelessServiceDescription()
+            {
+                ApplicationName = new Uri(appName),
+                PartitionSchemeDescription = new SingletonPartitionSchemeDescription(),
+                InstanceCount = 1,
+                ServiceTypeName = "MailboxType",
+                ServiceName = new Uri($"{appName}/test1")
+            };
+            return fc.ServiceManager.CreateServiceAsync(serviceDescription).ToString();
+        }
+
+        // POST api/values 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/ServiceManager/{serviceName}
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/ServiceManager/delete/{serviceName} 
+        public void Delete(int id)
+        {
+        }
+
+    }
+
+    /// <summary>  
+    /// Designed for use with JavaScriptSerializer.  
+    /// </summary>  
+    public class ServiceDefinition
+    {
+        public string id { get; set; }
+        public ServiceKind ServiceKind { get; set; }
+        public string Name { get; set; }
+        public string TypeName { get; set; }
+        public string ManifestVersion { get; set; }
+        public HealthState HealthState { get; set; }
+        public ServiceStatus ServiceStatus { get; set; }
+        public bool IsServiceGroup { get; set; }
+        public bool HasPersistedState { get; set; }
+    }
+
+
+}
---
+++ ServicesManager/ServicesManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace ServicesManager
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance. 
+    /// In some point, need to change to Stateful Service? 
+    /// </summary>
+    internal sealed class ServicesManager : StatelessService
+    {
+        public ServicesManager(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext => new OwinCommunicationListener(Startup.ConfigureApp, serviceContext, ServiceEventSource.Current, "ServiceEndpoint"))
+            };
+        }
+    }
+}
